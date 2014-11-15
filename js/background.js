926e3dce0cc3db3c1cff9d9a19f66692a2efdd0e
FelixMad <josefelixgarridoochoa@iMac-de-Jose.local>
2014-11-15 18:34:28 +0100

    0.0.5

--- js/background.js
+++ js/background.js
@@ -1,14 +1,15 @@
 function refreshLink(url){
 	var patternYoutube = /^https:\/\/www.youtube.com/i;
 	var patternVimeo = /^http[s]{0,1}:\/\/vimeo.com/i;
-	var icon = "../icon/inactive.png";
+	var file = "../img/"
+	var icon = file + "inactive.png";
 	var $u = false;
 	if(patternYoutube.test(url) || patternVimeo.test(url)){
 		for ($i in enlaces){
 			if(patternYoutube.test(url)) var $code = enlaces[$i].url.replace(/http[s]{0,1}:\/\/www.youtube.com\/watch\?v=([a-zA-Z0-9\-\_]{11})/, '$1');
 			if(patternVimeo.test(url)) var $code = enlaces[$i].url.replace(/vimeo.com\/(\d{5,10})/, '$1');
 			var patron = new RegExp($code);
-			if(patron.test(url)) var icon = "../icon/active.png";
+			if(patron.test(url)) var icon = file + "active.png";
 		}
 	}
 	chrome.browserAction.setIcon({path: icon });
